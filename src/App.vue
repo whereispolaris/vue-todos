07a0335af61c37b5285e21b3d17ac72caae14ec0
Santiago Enciso <whereispolaris@gmail.com>
2020-07-26 13:32:36 -0500

    Built logic for AddTodo component, installed uuid package.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <Header />
-    <AddTodo />
+    <AddTodo v-on:add-todo="addTodo" />
     <Todos v-bind:todos="todos" v-bind:key="type" v-on:del-todo="deleteTodo" />
   </div>
 </template>
@@ -41,6 +41,9 @@ export default {
   methods: {
     deleteTodo(id) {
       this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
     }
   }
 };
--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,14 +1,33 @@
 <template>
   <div>
-    <form>
-      <input type="text" name="title" placeholder="Add Todo..." />
+    <form @submit="addTodo">
+      <input type="text" v-model="title" name="title" placeholder="Add Todo..." />
       <input type="submit" value="Submit" class="btn" />
     </form>
   </div>
 </template>
 <script>
+import { v4 as uuidv4 } from "uuid";
 export default {
-  name: "AddTodo"
+  name: "AddTodo",
+  data() {
+    return {
+      title: ""
+    };
+  },
+  methods: {
+    addTodo(e) {
+      e.preventDefault();
+      const newTodo = {
+        id: uuidv4,
+        title: this.title,
+        completed: false
+      };
+      // Send up to parent
+      this.$emit("add-todo", newTodo);
+      this.title = "";
+    }
+  }
 };
 </script>
 <style scoped>
